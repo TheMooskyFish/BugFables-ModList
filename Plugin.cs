206b283e55026111a9faac08be8b8f2443cbe3c2
TheMooskyFish <TheMooskyFish@gmail.com>
2024-07-31 23:20:03 +0300

    v1.2

--- Plugin.cs
+++ Plugin.cs
@@ -2,8 +2,9 @@
 using BepInEx;
 using HarmonyLib;
 using BepInEx.Configuration;
+using System.Linq;
 
-[BepInPlugin("dev.mooskyfish.modlist", "Mod List", "1.1")]
+[BepInPlugin("dev.mooskyfish.modlist", "Mod List", "1.2")]
 [BepInProcess("Bug Fables.exe")]
 public class ModList : BaseUnityPlugin
 {
@@ -20,21 +21,20 @@ public class ModList : BaseUnityPlugin
     {
         static void Postfix(StartMenu __instance)
         {
-            var y = -3.3473f;
+            var y = -3.3f;
             var menu1 = (Transform)AccessTools.Field(typeof(StartMenu), "menu1").GetValue(__instance);
             if (menu1.transform.Find("Text: |size,0.45||halfline||color,4||font,0|v1.1.2"))
-            {
                 Destroy(menu1.transform.Find("Text: |size,0.45||halfline||color,4||font,0|v1.1.2").gameObject);
-            }
             MainManager.instance.StartCoroutine(MainManager.SetText("|size,0.45||halfline||color,4||font,0|v" + Application.version + $" - BepInEx v{typeof(Paths).Assembly.GetName().Version}", new Vector3(-8.75f, -3.55f, 10f), menu1));
             if (ShowModsCount.Value)
             {
-                MainManager.instance.StartCoroutine(MainManager.SetText($"|single||size,0.45||halfline||color,4||font,0|Mods Loaded: {BepInEx.Bootstrap.Chainloader.PluginInfos.Values.Count}", new Vector3(-8.75f, y, 10f), menu1));
+                var count = BepInEx.Bootstrap.Chainloader.PluginInfos.Values.Count(mod => mod.Instance.enabled);
+                MainManager.instance.StartCoroutine(MainManager.SetText($"|single||size,0.45||halfline||color,4||font,0|Mods Loaded: {count}", new Vector3(-8.75f, y, 10f), menu1));
                 return;
             }
             foreach (var mod in BepInEx.Bootstrap.Chainloader.PluginInfos.Values)
             {
-                if (mod.Metadata.Name == "Mod List")
+                if (mod.Metadata.Name == "Mod List" || !mod.Instance.enabled)
                     continue;
                 var name = mod.Metadata.Name;
                 string version;
@@ -44,7 +44,7 @@ public class ModList : BaseUnityPlugin
                 else
                     version = mod.Metadata.Version.ToString();
                 MainManager.instance.StartCoroutine(MainManager.SetText($"|single||size,0.45||halfline||color,4||font,0|{name} v{version}", new Vector3(-8.75f, y, 10f), menu1));
-                y += 0.28f;
+                y += 0.35f;
             }
         }
     }
